4748011ca05af191fe94a83e139052f928aca160
Rikin Chauhan <rikinchauhan01@gmail.com>
2022-03-05 12:48:19 +0530

    ContactService Implemented & Tested in BackendService

--- BackendService/BackendService/AccountService.cs
+++ BackendService/BackendService/AccountService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using System;
 using System.Data.SqlClient;
 using System.Web.Configuration;
 using BackendService.Models;
---
+++ BackendService/BackendService/IContactService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using BackendService.Models;
+
+namespace BackendService
+{
+    [ServiceContract]
+    public interface IContactService
+    {
+        [OperationContract]
+        List<Contact> GetContacts(int userId);
+
+        [OperationContract]
+        Contact AddContact(Contact contact);
+
+        [OperationContract]
+        Contact GetContact(Contact contact);
+
+        [OperationContract]
+        Contact UpdateContact(Contact contact);
+
+        [OperationContract]
+        Contact DeleteContact(Contact contact);
+
+        [OperationContract]
+        bool UploadToTempFolder(byte[] pFileBytes, string pFileName);
+
+        [OperationContract]
+        byte[] GetFileFromFolder(string pFileName);
+    }
+}
